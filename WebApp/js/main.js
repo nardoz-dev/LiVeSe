59e2c7763f0161a0cd4317e996f70e839947e470
nardoz-dev <nardomen96@gmail.com>
2022-07-21 15:54:06 +0200

    update dynamic table

--- WebApp/js/main.js
+++ WebApp/js/main.js
@@ -1,15 +1,26 @@
 
+const tableBody = document.querySelector('tbody');
 
+let out ="";
 async function init(){
     var result = await fetch("https://42lal2bea7b5ufnq42umzkhxxy0zuhki.lambda-url.eu-west-1.on.aws/")
         .then ( data => {return data.json(); })
         .then ( data => {
             data.forEach(element => {
-                console.log(element);
+                out +=  ` 
+                <tr>
+                    <td> ${element.devEUI.S} </td>
+                    <td> ${element.last_status.S} </td>
+                    <td> ${element.lat.S} </td>
+                    <td> ${element.long.S} </td>
+                    <td> ${element.status.S} </td>
+                    <td> ${element.street.S} </td>
+                    <td> ${element.timestamp.S} </td>
+                </tr>
+                ` ;
             });
+            tableBody.innerHTML = out;
         })
 
 }
-
-
 init();
