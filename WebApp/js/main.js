cfb7816152f35f2527db422850d0264f54000149
nardoz-dev <62448477+nardoz-dev@users.noreply.github.com>
2024-06-13 17:17:28 +0200

    Update main.js

--- WebApp/js/main.js
+++ WebApp/js/main.js
@@ -1,26 +1,55 @@
-
+// Table Variable
 const tableBody = document.querySelector('tbody');
+// Status Variable
+let _status = ""
 
-let out ="";
-async function init(){
-    var result = await fetch("https://42lal2bea7b5ufnq42umzkhxxy0zuhki.lambda-url.eu-west-1.on.aws/")
-        .then ( data => {return data.json(); })
-        .then ( data => {
-            data.forEach(element => {
-                out +=  ` 
-                <tr>
-                    <td> ${element.devEUI.S} </td>
-                    <td> ${element.last_status.S} </td>
-                    <td> ${element.lat.S} </td>
-                    <td> ${element.long.S} </td>
-                    <td> ${element.status.S} </td>
-                    <td> ${element.street.S} </td>
-                    <td> ${element.timestamp.S} </td>
-                </tr>
-                ` ;
-            });
-            tableBody.innerHTML = out;
-        })
+
+async function fetchData() {
+    try {
+        const response = await fetch("https://loreaqofqdepqcqo2fxd34fody0axwkn.lambda-url.eu-west-1.on.aws/");
+        if (!response.ok) {
+            throw new Error('Error during the retrieving of the data');
+        }
+        console.log("Retrieving data : Done")
+        return await response.json();
+    } catch (error) {
+        console.error('Error during the retrieving of the data:', error);
+        return [];
+    }
+}
+
+async function displayData() {
+    const data = await fetchData();
+    let output = "";
+    data.forEach(element => {
+        output += ` 
+            <tr>
+                <td>${element._devEUI.S}</td>
+                <td>${element.LastStatus.S}</td>
+                <td>${element.Status.S}</td>
+                <td>${element.Street.S}</td>
+                <td>${element.TimeStamp.S}</td>
+            </tr>`;
+        
+        if ( element.devEUI.S == "4" ){
+            _status = element.Status.S
+            console.log("_status dopo il displayData : ", _status)
+        }
+        
+    });
+    tableBody.innerHTML = output;
+   
+}
+
+async function main (){
+    // As First operation retrieve data : 
+    await displayData()
+  
+    setInterval(async () => {
+        currentIndex = (currentIndex + 1) % statusArray.length;
+        await displayData()
+    }, 10000); 
 
 }
-init();
+
+main()
